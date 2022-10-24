8db466894c63e56777382b2ffe4575f565d890a5
yanquanfahei <1449826851@qq.com>
2022-10-24 23:04:42 +0800

    feat: update param

--- 从前端到全栈/理解http/lib/aspect/param.js
+++ 从前端到全栈/理解http/lib/aspect/param.js
@@ -1,8 +1,29 @@
 import url from 'url'
-
+import queryString from 'query-string'
 export default async function(ctx, next) {
   const { req } = ctx
   const { query } = url.parse(`http://${req.headers.host}${req.url}`)
-  ctx.params = new url.URLSearchParams(query)
+  ctx.params = queryString.parse(query)
+
+  if(req.method === 'POST') {
+    const headers = req.headers
+    // 读取POST的body数据
+    const body = await new Promise((resolve) => {
+      let data = ''
+      req.on('data', (chunk) => {
+        data += chunk.toString() // convert Buffer to string
+      });
+      req.on('end', () => {
+        resolve(data)
+      });
+    });
+    ctx.params = ctx.params || {}
+    if(headers['content-type'] === 'application/x-www-form-urlencoded') {
+      Object.assign(ctx.params, queryString.parse(body))
+    } else if(headers['content-type'] === 'application/json') {
+      Object.assign(ctx.params, JSON.parse(body))
+    }
+  }
+  
   await next()
 }
