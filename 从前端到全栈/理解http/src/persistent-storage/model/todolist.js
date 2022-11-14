a54c2f16edd967cb8e54b6c2adb60942cf6c513b
yanquanfahei <1449826851@qq.com>
2022-11-14 21:08:24 +0800

    feat: session

--- 从前端到全栈/理解http/src/persistent-storage/model/todolist.js
+++ 从前端到全栈/理解http/src/persistent-storage/model/todolist.js
@@ -1,11 +1,11 @@
-export async function getList(database) {
-  const result = await database.all('SELECT * FROM todo')
-  return result
+export async function getList(database, userInfo) {
+  const result = await database.all(`SELECT * FROM todo WHERE state <> 2 and userid = ${userInfo.id} ORDER BY state DESC`);
+  return result;
 }
 
-export async function addTask(database, {text, state}) {
+export async function addTask(database, userInfo, {text, state}) {
   try {
-    const data = await database.run('INSERT INTO todo(text,state) VALUES (?, ?)', text, state);
+    const data = await database.run('INSERT INTO todo(text,state, userid) VALUES (?, ?, ?)', text, state, userInfo.id);
     return {err: '', data};
   } catch (ex) {
     return {err: ex.message};
--- 从前端到全栈/理解http/src/persistent-storage/server/index.js
+++ 从前端到全栈/理解http/src/persistent-storage/server/index.js
@@ -5,9 +5,12 @@ import { open } from 'sqlite'
 import { Server, Router } from '../../../lib/index.js'
 import { getList, addTask } from '../model/todolist.js'
 import param from '../../../lib/aspect/param.js'
+import cookie from '../../../lib/aspect/cookie.js'
 import { existsSync, statSync, createReadStream } from 'fs'
 import mime from 'mime'
 import zlib from 'zlib'
+import { login } from '../model/user.js'
+import { getSession } from '../model/session.js'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const dbFile = resolve(__dirname, '../database/todolist.db') // todolist.db是sqlite数据库文件
@@ -17,10 +20,24 @@ let db = null
 const app = new Server()
 const router = new Router()
 
+app.use(cookie)
+app.use(param)
+
+
+app.use(async ({cookies, res}, next) => {
+  let id = cookies.interceptor_js;
+  if(!id) {
+    id = Math.random().toString(36).slice(2);
+  }
+  res.setHeader('Set-Cookie', `interceptor_js=${id}; Path=/; Max-Age=${7 * 86400}`); // 设置cookie的有效时长一周
+  await next();
+});
+
 app.use(async ({ req }, next) => {
   console.log(`${req.method} ${req.url}`)
   await next()
 })
+
 
 app.use(async (ctx, next) => {
   if(!db) { // 如果数据库连接未创建，就创建一个
@@ -34,23 +51,55 @@ app.use(async (ctx, next) => {
   await next()
 });
 
+app.use(router.post('/login', async (ctx, next) => {
+  const {database, params, res} = ctx;
+  res.setHeader('Content-Type', 'application/json');
+  const result = await login(database, ctx, params);
+  res.statusCode = 302;
+  if(!result) { // 登录失败，跳转到login继续登录
+    res.setHeader('Location', '/login.html');
+  } else {
+    res.setHeader('Location', '/'); // 成功，跳转到 index
+  }
+  await next();
+}));
+
+async function checkLogin(ctx) {
+  const userInfo = await getSession(ctx.database, ctx, 'userInfo');
+  ctx.userInfo = userInfo;
+  return ctx.userInfo;
+}
+
 /*
 如果请求的路径是/list，则从todo表中获取所有任务数据
 */
-app.use(router.get('/list', async ({database, route, res}, next) => {
-  res.setHeader('Content-Type', 'application/json')
-  const result = await getList(database) // 获取任务数据
-  res.body = {data: result}
-  await next()
+app.use(router.get('/list', async (ctx, next) => {
+  const { res } = ctx
+  const userInfo = await checkLogin(ctx); // 如果session存在并有效，则返回用户信息对象
+  res.setHeader('Content-Type', 'application/json');
+
+  if(userInfo) {
+    const result = await getList(database, userInfo);
+    res.body = {data: result};
+  } else {
+    res.body = {err: 'not login'};
+  }
+  await next();
 }))
 
-app.use(param)
-
-app.use(router.post('/add', async ({database, params, res}, next) => {
+app.use(router.post('/add', async (ctx, next) => {
+  const {database, params, res} = ctx;
+  const userInfo = await checkLogin(ctx);
   res.setHeader('Content-Type', 'application/json');
-  const result = await addTask(database, params)
-  res.body = result
-  await next()
+
+  if(userInfo) {
+    const result = await addTask(database, userInfo, params);
+    res.body = result;
+    await next();
+  } else {
+    res.body = {err: 'not login'};
+  }
+  await next();
 }));
 
 
