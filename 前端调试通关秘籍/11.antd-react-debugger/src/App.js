2b8e298bdf4e204bf57e6825eb871f3c654d10c3
yanquanfahei <1449826851@qq.com>
2022-11-27 20:39:29 +0800

    feat: antd debugger

---
+++ 前端调试通关秘籍/11.antd-react-debugger/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Button } from './dist2/antd.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">
+        按钮
+      </Button>
+    </div>
+  );
+}
+
+export default App;
