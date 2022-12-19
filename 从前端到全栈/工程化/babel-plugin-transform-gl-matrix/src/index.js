6fcb94f66d73f9b73e600b53810f29fb50309519
yanquanfahei <1449826851@qq.com>
2022-12-19 21:10:16 +0800

    feat: babel plugin next

--- 从前端到全栈/工程化/babel-plugin-transform-gl-matrix/src/index.js
+++ 从前端到全栈/工程化/babel-plugin-transform-gl-matrix/src/index.js
@@ -1,5 +1,6 @@
-module.exports = function ({ types: t }) {
+module.exports = function babelPluginTransformGlMatrix({ types: t }) {
   // plugin contents
+
   return {
     visitor: {
       CallExpression: {
@@ -9,6 +10,25 @@ module.exports = function ({ types: t }) {
             const args = path.node.arguments;
             if (args.length === 2) {
               path.node.callee.name = 'vec2.fromValues';
+            }
+          }
+        },
+      },
+      UnaryExpression: {
+        exit(path) {
+          const { operator, argument } = path.node;
+          if (operator === '-') {
+            if (t.isCallExpression(argument) && argument.callee.name === 'vec2') {
+              const node = t.callExpression(
+                t.identifier(`${argument.callee.name}.negate`),
+                [
+                  t.callExpression(
+                    t.identifier(`${argument.callee.name}.create`),
+                    [],
+                  ),
+                  argument.arguments[0]],
+              );
+              path.replaceWith(node);
             }
           }
         },
@@ -33,6 +53,65 @@ module.exports = function ({ types: t }) {
                 );
                 path.replaceWith(node);
               }
+            } else if (t.isNumericLiteral(right)) {
+              const { operator } = path.node;
+              if (operator === '+') {
+                // https://babeljs.io/docs/en/babel-types
+                const node = t.callExpression(
+                  t.identifier(`${left.callee.name}.add`),
+                  [
+                    t.callExpression(
+                      t.identifier(`${left.callee.name}.create`),
+                      [],
+                    ),
+                    left.arguments[0],
+                    t.callExpression(
+                      t.identifier(`${left.callee.name}.fromValues`),
+                      [right, t.numericLiteral(0)],
+                    )],
+                );
+                path.replaceWith(node);
+              }
+            } else {
+              // console.log(right);
+            }
+          } else if (t.isCallExpression(left) && left.callee.name === 'vec2.add') {
+            if (t.isCallExpression(right) && right.callee.name === 'vec2') {
+              const { operator } = path.node;
+              if (operator === '+') {
+                // https://babeljs.io/docs/en/babel-types
+                const node = t.callExpression(
+                  t.identifier(left.callee.name),
+                  [
+                    t.callExpression(
+                      t.identifier(`${right.callee.name}.create`),
+                      [],
+                    ),
+                    left,
+                    right.arguments[0]],
+                );
+                path.replaceWith(node);
+              }
+            }
+          } else if (t.isNumericLiteral(left) && t.isCallExpression(right) && right.callee.name === 'vec2') {
+            const { operator } = path.node;
+            if (operator === '+') {
+              // https://babeljs.io/docs/en/babel-types
+              const node = t.callExpression(
+                t.identifier(`${right.callee.name}.add`),
+                [
+                  t.callExpression(
+                    t.identifier(`${right.callee.name}.create`),
+                    [],
+                  ),
+                  t.callExpression(
+                    t.identifier(`${right.callee.name}.fromValues`),
+                    [left, t.numericLiteral(0)],
+                  ),
+                  right.arguments[0],
+                ],
+              );
+              path.replaceWith(node);
             }
           }
         },
